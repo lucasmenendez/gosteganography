c77d7ba08a012edb1f694fdb4e378604e4acd5cc
Lucas Menéndez <hi@lucasmenendez.me>
2023-04-19 22:01:18 +0200

    pixel.go comments

--- pixel.go
+++ pixel.go
@@ -5,15 +5,23 @@ package gosteganography
 
 import "image/color"
 
+// pixel struct contains the coordenates of the pixel and its color
+// (color.Color) reference
 type pixel struct {
 	x, y  int
 	color color.Color
 }
 
+// pixels is the type abstraction for a list of pointers to pixels
 type pixels []*pixel
 
+// from function fills the current pixels with the pixels of the provided Image.
+// It also returns the filled pixels.
 func (ps pixels) from(img *Image) pixels {
+	// get original image bounds from Image provided
 	bounds := img.original.Bounds()
+	// iterates over every original image pixels storing their colors and
+	// coordenates
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			ps = append(ps, &pixel{x, y, img.original.At(x, y)})
@@ -22,8 +30,17 @@ func (ps pixels) from(img *Image) pixels {
 	return ps
 }
 
+// writebin function writes the provided binary slice  into current pixels. It
+// iterates over the pixels writing the bits three by three, one per pixel color
+// component. To write a bit, it takes the binary representation of each color
+// component and replaces the less important bit with each bit.
 func (ps pixels) writebin(bin []uint) pixels {
+	// create some variables to keep track of the bits written, to avoid writing
+	// unnecessary pixels or components, only the required number of bits.
 	bit, maxbits := 0, len(bin)
+	// iterate over the pixels, changing the same number of pixel components
+	// as the number of bits of the binary slice. The rest of pixels remain
+	// the same.
 	for n, p := range ps {
 		r, g, b, a := p.color.RGBA()
 		if bit < maxbits {
@@ -50,6 +67,9 @@ func (ps pixels) writebin(bin []uint) pixels {
 	return ps
 }
 
+// readbin functions returns the binary representation of a hidden message into
+// the current pixels. It returns the less important bits of the n first pixel
+// colors components.
 func (ps pixels) readbin(nbits int) []uint {
 	bin := []uint{}
 	currentBit := 0
